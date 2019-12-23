1b492550cbcd11ac491ecd68f2b7b8de0ba90998
james.kim <james.kim@yht.co.kr>
2019-12-23 16:19:12 +0900

    study

---
+++ 6-userRESTAPI/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/users', require('./api/user'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!\n');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
